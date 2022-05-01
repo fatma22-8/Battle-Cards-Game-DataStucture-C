784c2a1cef1bbfa9db27043a32ad48c7d666f4d0
Fatma Adel <68230239+fatma22-8@users.noreply.github.com>
2022-05-01 12:06:18 +0200

    Update cards.c

--- cards.c
+++ cards.c
@@ -11,162 +11,98 @@ Card* createCard(){
 
  int randomnumber = 0;
 
-  randomnumber = (rand() % 10) +1;
+     randomnumber = (rand() % 10) +1;
 
- if(randomnumber>0 && randomnumber <=4)//40% chances for attack
-    {
-        card->card_type=0;
-        card->value= (rand() % (10)) + 1;
-    }
+    if(randomnumber>0 && randomnumber <=4){//40% chances for attack
 
- else if(randomnumber>4 && randomnumber <=9)//50% chances for DEFEND
-    {
-        card->card_type=1;
+        card->card_type=ATTACK;
         card->value= (rand() % 10) + 1;
 
     }
+    else if(randomnumber>4 && randomnumber <=9){//50% chances for DEFEND
 
- else //10% chances for RUN
-    {
-        card->card_type=2;
+        card->card_type=DEFEND;
+        card->value= (rand() % 10) + 1;
+
+    }
+     else{ //10% chances for RUN
+
+        card->card_type=RUN;
         card->value= (rand() % 10) + 1;
     }
-
-card->next=NULL;
-
+    card->next=NULL;
 return card;
 }
 
-Card* removeCard( Card *head,Card*p )
-{
-   Card *toDelete;
-   if(head==p)
+Card* removeCard( Card *head ){
+
+     Card *toDelete;
+
+    if(head != NULL)
     {
-        toDelete=head;
+           toDelete = head;
         head = head->next;
         free(toDelete);
-    }
-
-    else
-    {
-        toDelete=head;
-        while(toDelete->next!=p)
-            toDelete=toDelete->next;
-
-        if(p->next==NULL)
-            {
-                free(p);
-                toDelete->next=NULL;
-            }
-        else
-            {
-                toDelete->next=p->next;
-                free(p);
-            }
-    }
-
+ }
 return head;
 }
 
-Card* addCard( Card *head, Card *c )
-{
+Card* addCard( Card *head, Card *c ){
+
     Card* current;
 
-    if (head == NULL || (head)->value <= c->value)
-        {
-            c->next = head;
-            head = c;
-        }
+    if (head == NULL || (head)->value <= c->value) {
+        c->next = head;
+        head = c;
+    }
     else {
-            current = head;
-            while (current->next != NULL && current->next->value > c->value)
-                            current = current->next;
-
+        current = head;
+        while (current->next != NULL && current->next->value > c->value) {
+            current = current->next;
+        }
         c->next = current->next;
         current->next = c;
-         }
+    }
 return head;
 }
 
-int getLength( Card *head )
-{
+int getLength( Card *head ){
      int num = 0;
     while (head != NULL)
     {
         num += 1;
         head = head->next;
     }
-return num;
+    return num;
 }
 
-void printCard( Card *head )
-{
+void printCard( Card *head ){
     char cardT;
-  if(head->card_type==0)
-     cardT='A';
-
-  else if(head->card_type==1)
+if(head->card_type==0){
+    cardT='A';
+}else if(head->card_type==1){
     cardT='D';
-
-  else
+}else{
     cardT='R';
-
+}
 printf("%c%d ", cardT, head->value);
 }
 
-void printCards( Card *head )
-{
-     for ( ; head != NULL; head = head->next )
+void printCards( Card *head ){
+
+ for ( ; head != NULL; head = head->next )
        printCard(head);
-
-printf( "\n" );
-
+    
+    printf( "\n" );
 }
 
-Card* buildCards(int n )
-{
-int i;
+Card* buildCards(int n ){
+    int i;
+    Card* head=NULL;
 
-Card* head=NULL;
-
- for(i=0 ; i<n; i++)
-    head= addCard(head, createCard());
-
+    for(i=0 ; i<n; i++){
+   head= addCard(head, createCard());
+    }
 return head;
 }
-
-Card*Search(Card*head,int type,int value)
-{
-    Card*current=head;
-
-    while(current->next!=NULL)
-    {
-        if(current->value==value&&current->card_type==type)
-            return current;
-
-        current=current->next;
-    }
-    if(current->next==NULL)
-    {
-       if(current->value==value&&current->card_type==type)
-            return current;
-    }
- return NULL;
-}
-
-Card*Pullcard(Card*p1_head,char type,int val)
-{
-    int cardt;
-    if(type=='A' || type=='a' )
-            cardt=0;
-
-     if(type=='D'|| type=='d')
-            cardt=1;
-
-     if(type=='R'|| type=='r')
-            cardt=2;
-
-return Search(p1_head,cardt,val);
-}
-
 
