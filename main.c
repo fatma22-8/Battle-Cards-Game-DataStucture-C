207a55e86b784895a8b20eedd4aef0b7c5dbc032
Fatma Adel <68230239+fatma22-8@users.noreply.github.com>
2022-05-01 11:57:58 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include<time.h>
 #include "cards.h"
+
 int main()
 {
-
 
 	clock_t start = clock();
 
@@ -16,7 +16,7 @@ int main()
     srand(time(NULL));
 
 	printf("Enter number of cards:    ");
-scanf("%d",&n);
+        scanf("%d",&n);
 
  p1=buildCards(n);
  p2=buildCards(n);
@@ -33,14 +33,14 @@ scanf("%d",&n);
 
  printCards(p2);
 
-
 for(;; ){
 
-    if(getLength(p1) ==0 || getLength(p2) ==0){
+      if(getLength(p1) ==0){
+        printf("\n\n\n============ GAME OVER =============\n\n\n\n");
 
-      printf("\n\n\n============ GAME OVER =============\n\n\n\n");
+            printf("\nPlayer 1 ran out of cards. Player 2 wins\n\n");
 
-      printf("Player 1 ending cards: ");
+    printf("Player 1 ending cards: ");
 
       printCards(p1);
 
@@ -48,14 +48,20 @@ for(;; ){
 
       printCards(p2);
 
-      if(getLength(p1) ==0){
-
-        printf("\nPlayer 1 ran out of cards. Player 2 wins\n\n");
 
       }else{
 
-        printf("\nPlayer 2 ran out of cards. Player 1 wins\n\n");
 
+ printf("\n\n\n============ YOU WIN =============\n\n\n\n");
+
+
+    printf("Player 1 ending cards: ");
+
+      printCards(p1);
+
+      printf("Player 2 ending cards: ");
+
+      printCards(p2);
       }
 
       printf("The end\n");
@@ -66,6 +72,7 @@ for(;; ){
 
  return 0;
     }
+
     printf("\n----- ROUND %d -----\n", round);
 
     printf("\nplayer 1: (%d)", getLength(p1) );
@@ -77,6 +84,7 @@ for(;; ){
     printCard(p2);
 
     /***********/
+
     if(p1->card_type==0 && p2->card_type==0){//attack
 
         printf("\nBoth players ATTACK\n");
@@ -85,32 +93,29 @@ for(;; ){
 
            p1= removeCard(p1);
 
-           p2 = removeCard(p2);
-           p2 = removeCard(p2);
-
            p1=addCard(p1, createCard());
 
-           printf("\n Player 1 wins and gets a new card. Player 2 loses their next card into the abyss.\n");
+           p2 = removeCard(p2);
+
+           printf("\n Player 1 gets a new card.\n");
 
         }else if(p2->value > p1->value){
 
-           p1= removeCard(p1);
-           p1= removeCard(p1);
-
-           p2 = removeCard(p2);
+         p2= removeCard(p2);
 
            p2=addCard(p2, createCard());
 
-           printf("\nPlayer 2 wins and gets a new card. Player 1 loses their next card into the abyss.\n");
+           p1 = removeCard(p1);
+
+           printf("\nPlayer 2 gets a new card.\n");
 
         }else if(p1->value == p2->value){
 
          p1= removeCard(p1);
 
          p2= removeCard(p2);
+           printf("\nNothing happens\n"); //No winner for this round
         }
-
-
     }
      /***********/
      else if(p1->card_type==1 && p2->card_type==1){//defend
@@ -127,9 +132,9 @@ for(;; ){
 
     else if(p1->card_type==2 && p2->card_type==2){//run
 
-     printf("\nBoth players RUN \n");
+      printf("\nBoth players RUN \n");
 
-     printf("Both players lose their next cards into the abyss.\n");
+      printf("\nBoth players lose their next cards.\n");
 
       p1= removeCard(p1);
 
@@ -142,35 +147,29 @@ for(;; ){
 
     else if(p1->card_type==0 && p2->card_type==1){//attacking vs defending
 
-      printf("\nPlayer 1 ATTACKs player 2 DEFENDs  \n");
+        printf("\nPlayer 1 ATTACKs player 2 DEFENDs  \n");
 
         if(p1->value>p2->value){
 
-            //p1 wins
-
-            printf("\nPlayer 1 wins. Player 1 gets a new card.  \n");
+printf("\nPlayer 1 gets a new card. Player 2 loses his next card. \n");
 
             p1= removeCard(p1);
 
             p2 = removeCard(p2);
+            p2 = removeCard(p2);
 
             p1=addCard(p1, createCard());
 
-        }else if(p2->value>=p1->value){
+        }
+        else if(p2->value>=p1->value){
 
-             printf("Player 1 loses their next card into the abyss.\n");
-
-            //p2 wins
-             p1= removeCard(p1);
+             printf("\nNothing happens.\n");;
 
              p1= removeCard(p1);
 
              p2 = removeCard(p2);
-
         }
-
     }
-
 
     else if(p2->card_type==0 && p1->card_type==1){//attacking vs defending
 
@@ -178,9 +177,8 @@ for(;; ){
 
         if(p2->value > p1->value){
 
-            //p1 wins
-
             p1= removeCard(p1);
+             p1= removeCard(p1);
 
             p2 = removeCard(p2);
 
@@ -188,7 +186,7 @@ for(;; ){
 
             if(getLength(p1) ==1){
 
-              printf("Player 1 loses and Player 2 survives\n");
+               printf("\nPlayer 2 gets a new card. Player 1 loses his next card.  \n");
 
             }
             else if(getLength(p1) ==1){
@@ -200,14 +198,11 @@ for(;; ){
 
         }else{
 
-
-
-            //p2 wins
              p1= removeCard(p1);
 
-             p2= removeCard(p2);
-
              p2 = removeCard(p2);
+
+              printf("\nNothing happens.\n");
 
              if(getLength(p1) ==1){
 
@@ -218,18 +213,15 @@ for(;; ){
 
               printf("Player 2 loses and Player 1 survives\n");
             }
-
-
              printf("Player 2 loses their next card into the abyss.\n");
-
-
         }
-
     }
     /***********/
     else if(p1->card_type==0 && p2->card_type==2 ){//attacking vs running
 
        p1= removeCard(p1);
+
+       p1=addCard(p1, createCard());
 
        p2= removeCard(p2);
 
@@ -237,57 +229,48 @@ for(;; ){
 
        printf("\nPlayer 1 ATTACKs and Player 2 RUNs.\n");
 
-       printf("Player 2 loses their next card into the abyss.\n");
-
-
+        printf("\nPlayer 1 gets a new card. Player 2 loses his next card.\n");
     }
      /***********/
      else if(p2->card_type==0 && p1->card_type==2){
 
-      p1= removeCard(p1);
-
-      p1 = removeCard(p1);
-
       p2= removeCard(p2);
+
+       p2=addCard(p2, createCard());
+
+       p1= removeCard(p1);
+
+       p1 = removeCard(p1);
 
       printf("Player 2 ATTACKs and Player 1 RUNs.\n");
 
-      printf("Player 1 loses their next card into the abyss.\n");
+     printf("\nPlayer 2 gets a new card. Player 1 loses his next card.\n");
     }
-
     /***********/
-    /***********/
-    else if(p1->card_type==1 && p2->card_type==2 ){//attacking vs running
+    else if(p1->card_type==1 && p2->card_type==2 ){//defending vs running
 
-        printf("Player 1 gets a new card. Player 2 loses their next card into the abyss.\n");
+        printf("\nPlayer 1 DEFENDs and Player 2 RUNs.\n");
+
+       printf("\nPlayer 2 loses his next card.\n");
 
         p1 = removeCard(p1);
 
         p2= removeCard(p2);
-
-        p1=addCard(p1, createCard());
 
         p2= removeCard(p2);
 
     }else if(p2->card_type==1 && p1->card_type==2){
 
-      printf("Player 2 gets a new card. Player 1 loses their next card into the abyss.\n");
+     printf("\nPlayer 2 DEFENDSs and Player 1 RUNs.\n");
+
+    printf("\nPlayer 1 loses his next card .\n");
 
         p1 = removeCard(p1);
 
         p2= removeCard(p2);
 
-        p2=addCard(p2, createCard());
-
         p1= removeCard(p1);
     }
-    /***********/
-    /***********/
-
-
     round++;
-
-}
-
  return 0;
  }
